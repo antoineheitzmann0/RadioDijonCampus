753ae2ddac68c784174666705c795bf8bf08eaa1
Antoine <antoineheitzmann@outlook.fr>
2020-02-20 15:12:56 +0100

    Ajout des classes

---
+++ RDC/RDC/Model/Show.swift
@@ -0,0 +1,13 @@
+//
+//  Show.swift
+//  RDC
+//
+//  Created by Antoine Heitzmann on 20/02/2020.
+//  Copyright © 2020 RadioDijonCampus. All rights reserved.
+//
+
+import UIKit
+
+public class Show: NSObject {
+
+}
